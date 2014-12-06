4698e2ab4776a95fcf0457e820582c3f5402082f
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2014-12-06 22:05:30 +0700

    test alien is a Phaser.Sprite

--- spec/AlienSpec.js
+++ spec/AlienSpec.js
@@ -1,22 +1,39 @@
 describe('Alien', function() {
-  var alien;
 
-  beforeEach(function() {
-    alien = new Alien();
+  describe('create', function() {
+    it('should call Phaser.Sprite', function() {
+      spyOn(Phaser.Sprite, 'call');
+
+      var alien = new Alien({}, {x:0, y:0}, 'alien');
+
+      expect(Phaser.Sprite.call).toHaveBeenCalledWith(alien, {}, 0, 0, 'alien', 0);
+    });
   });
 
-  it('should move left', function() {
-    alien.moveLeft();
-    expect(alien.body.velocity.x).toEqual(-200);
-  });
+  describe('action', function() {
+    var alien;
 
-  it('should move right', function() {
-    alien.moveRight();
-    expect(alien.body.velocity.x).toEqual(200);
-  });
+    beforeEach(function() {
+      spyOn(Phaser.Sprite, 'call');
+      alien = new Alien({}, 0, 0, 'alien');
+      alien.body = {
+        velocity: { x: 0 }
+      };
+    });
 
-  it("should don't move", function() {
-    alien.doNotMove();
-    expect(alien.body.velocity.x).toEqual(0);
+    it('should move left', function() {
+      alien.moveLeft();
+      expect(alien.body.velocity.x).toEqual(-200);
+    });
+
+    it('should move right', function() {
+      alien.moveRight();
+      expect(alien.body.velocity.x).toEqual(200);
+    });
+
+    it("should don't move", function() {
+      alien.doNotMove();
+      expect(alien.body.velocity.x).toEqual(0);
+    });
   });
 });
--- src/Alien.js
+++ src/Alien.js
@@ -1,26 +1,17 @@
-function Alien() {
+function Alien(game, position, name) {
+  Phaser.Sprite.call(this, game, position.x, position.y, name, 0);
 }
 
+Alien.prototype = Object.create(Phaser.Sprite.prototype);
+
 Alien.prototype.moveLeft = function() {
-  this.body = {
-    velocity: {
-      x: -200
-    }
-  };
+  this.body.velocity.x = -200;
 };
 
 Alien.prototype.moveRight = function() {
-  this.body = {
-    velocity: {
-      x: 200
-    }
-  };
+  this.body.velocity.x = 200;
 };
 
 Alien.prototype.doNotMove = function() {
-  this.body = {
-    velocity: {
-      x: 0
-    }
-  };
+  this.body.velocity.x = 0;
 };
